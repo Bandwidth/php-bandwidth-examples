8871f9e903ae292de249a812b9e3521cbf9bc7e2
Nadir Hamid <matrix.nad@gmail.com>
2015-03-16 20:26:58 +0000

    test heroku postgres

--- test.heroku.php
+++ test.heroku.php
@@ -2,9 +2,10 @@
 
 require_once("./lib/sqlite3fallback.php");
 
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
 
-$res = exec("heroku config:get DATABASE_URL");
-$db = new SQLite3Fallback2($res);
-
-echo var_dump($db);
+$db = new SQLite3Fallback2(pg_connection_string_from_database_url());
 ?>
