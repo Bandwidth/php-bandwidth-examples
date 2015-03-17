a7871c6a79f90440ebbf4ba6e58e71e8ceffaad0
Nadir Hamid <matrix.nad@gmail.com>
2015-03-17 00:53:11 +0000

    heroku tests

--- app-03-voice-reminders/helpers.php
+++ app-03-voice-reminders/helpers.php
@@ -61,6 +61,6 @@ function makeDate($data) {
  * getting the array values (application.json)
  */
 function titlecase($str) {
-  return substr(strtoupper($str, 0, 1)) . substr(strtolower($str, 1, strlen($str) - 1));
+  return strtoupper(substr($str, 0, 1)) . strtolower(substr(($str, 1, strlen($str) - 1));
 
 }
